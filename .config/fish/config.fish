bf064f749942932d7f667df77b67e4b585d2f513
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2026-05-29 18:27:15 +0200

    Add new aliases to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,14 @@ alias gc='git commit'
 alias gp='git pull'
 alias gsm='git switch $(git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@")'
 alias zed='zeditor'
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias clipboard='xclip -selection c'
+alias sdocs='rg -w ~/Documents -e ' # search ~/Documents and print path to file
+alias sodocs='sdocs $1 | awk {grep pwd} | vim'
+alias osdocs='cd ~/Documents && fzf > fzfout && vim $(cat fzfout)' # search ~/Documents with fzf and open file in vim
+
 
 set -gx PATH $PATH $HOME/bin
 # overwrite greeting
